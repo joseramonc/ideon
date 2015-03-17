455adddcce5b02433330c13d9252b81f5677bcf1
Jose Ramon Camacho <joserracamacho@gmail.com>
2015-03-17 10:51:02 -0600

    redirect to unauthorized when no token

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -6,7 +6,7 @@ module Api
 
     def authenticate_with_token
       auth_token = request.headers["Authorization"]
-      if user = User.find_by_auth_token(auth_token)
+      if auth_token && user = User.find_by_auth_token(auth_token)
         sign_in(:user, user)
       else
         head :unauthorized
