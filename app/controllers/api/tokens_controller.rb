f4916883feb52fb55723a91813d00a8e8c6053e7
Jose Ramon <joserracamacho@gmail.com>
2015-03-23 09:15:35 -0600

    Update tokens_controller.rb

--- app/controllers/api/tokens_controller.rb
+++ app/controllers/api/tokens_controller.rb
@@ -1,7 +1,8 @@
 class Api::TokensController < ActionController::Base
 
   def create
-    if @user = User.find_by_email(params[:email]) && @user.valid_password?(params[:password])
+    @user = User.find_by_email(params[:email])
+    if  @user && @user.valid_password?(params[:password])
       @user.ensure_or_generate_token
     else
       head :unauthorized
